14261db5068515efdd0d9eec1bdd50f889f19128
Divya <divyasaraswathi2004@gmail.com>
2026-04-19 15:15:54 +0530

    Initial commit

---
+++ src/main/java/com/dg/dg_heritage/DgHeritageApplication.java
@@ -0,0 +1,13 @@
+package com.dg.dg_heritage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgHeritageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgHeritageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dg/dg_heritage/DgHeritageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dg.dg_heritage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgHeritageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
